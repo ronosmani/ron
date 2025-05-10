8054a59d4f65b5e4217bf928d41de0bff83f5188
ronosmani <ronosmani704@gmail.com>
2025-05-10 04:29:32 -0700

    day22

---
+++ day22/main.js
@@ -0,0 +1,61 @@
+document.write("hello world");
+
+var x = 10;
+var name = "adi baba";
+
+document.write(x);
+console.log(name);
+
+var a = 50 + 50;
+console.log(a);
+
+var y = 50;
+var z = 80;
+var yz = y + z;
+
+console.log(yz);
+
+var n = 88;
+n++;
+console.log(n);
+
+var o = 40;
+o--;
+console.log(o);
+
+var p = 20;
+var q = p ** 2;
+console.log(q);
+
+var m = 15;
+m+=5; // 15 + 5 = 20
+console.log(m);
+
+var r = 35;
+r*=5;
+console.log(r);
+
+var num1 = 5;
+var num3 = 10;
+var num2 = 5;
+console.log(num1 != num2);
+console.log(num1 == num2);
+console.log(num1 > num2);
+console.log(num1 >= num2);
+console.log(num1 < num2);
+console.log(num1 <= num2);
+
+var firstName = "Digital";
+var lastName = "School";
+
+var fullName = firstName + " " + lastName;
+console.log(fullName);
+
+var test = "Test123";
+console.log*(`Hello from ${test}`);
+
+var age = 22;
+var age2 = 24;
+console.log(age >= 18 && age<=65);
+console.log(age >= 28 || age<=65);
+console.log(!(age == age2));
